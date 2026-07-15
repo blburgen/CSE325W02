159937118ca357782f3e4e8e0635d8d8c1056f27
Brady Burgener <bradylbsee@gmail.com>
2026-07-14 22:36:07 -0600

    completed part 5

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,76 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                    Price = 7.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                    Price = 8.99M
+                },
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                    Price = 9.99M
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western",
+                    Price = 3.99M
+                },
+                new Movie
+                {
+                    Title = "Dracula: Dead and Loving It",
+                    ReleaseDate = DateTime.Parse("1995-12-22"),
+                    Genre = "Horror Comedy",
+                    Price = 3.99M
+                },
+                new Movie
+                {
+                    Title = "Get Smart",
+                    ReleaseDate = DateTime.Parse("2008-06-20"),
+                    Genre = "Action Comedy",
+                    Price = 3.99M
+                },
+                new Movie
+                {
+                    Title = "The Lord of the Rings: The Fellowship of the Ring",
+                    ReleaseDate = DateTime.Parse("2001-12-19"),
+                    Genre = "Fantasy Adventure",
+                    Price = 8.61M
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
